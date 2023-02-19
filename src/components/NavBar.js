6b99440e76dbc861d21dc974ef65d0ffab2448ce
JAMES <jameskibunja17@gmail.com>
2023-02-19 14:14:08 +0300

    Done Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,10 +5,10 @@ function NavBar() {
   return (
   <div className="navbar">
     <ul>
-    <NavLink to="/" exact>Home</NavLink>
-    <NavLink to="/movies"exact>Movies</NavLink>
-    <NavLink to="/directors"exact>Directors</NavLink>
-    <NavLink to="/actors"exact>Actors</NavLink>
+   <li><NavLink to="/" exact>Home</NavLink></li> 
+   <li><NavLink to="/movies"exact>Movies</NavLink></li>
+   <li><NavLink to="/directors"exact>Directors</NavLink></li>
+   <li><NavLink to="/actors"exact>Actors</NavLink></li>
     </ul>
      
   </div>
